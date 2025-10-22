f2f7340116f87ae4a8090da7e90c74980f1130f4
ezgiefsa <250541023@firat.edu.tr>
2025-10-22 12:25:43 +0300

    Add files via upload

---
+++ Task-3-GeometrikHesaplamalar/dikdörtgen.java
@@ -0,0 +1,13 @@
+
+public class dikdörtgen {
+	public static void main(String[] args) {
+		double en = 4.5;
+		double boy = 7.9;
+		double alan = en*boy;
+		double cevre = (2 * en)+(2 * boy);
+		System.out.println("Alan: " + alan);
+		System.out.println("Cevre: " + cevre);
+		
+	}
+
+}
