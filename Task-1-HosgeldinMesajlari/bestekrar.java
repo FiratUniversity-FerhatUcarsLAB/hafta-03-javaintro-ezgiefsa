33eaeacfc8b02b6109d1d741425918c76dd1ea67
ezgiefsa <250541023@firat.edu.tr>
2025-10-22 12:00:21 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/bestekrar.java
@@ -0,0 +1,10 @@
+
+public class bestekrar {
+	public static void main(String[] args) {
+		System.out.println("Java ’ya Hos Geldiniz");
+		System.out.println("Java ’ya Hos Geldiniz");
+		System.out.println("Java ’ya Hos Geldiniz");
+		System.out.println("Java ’ya Hos Geldiniz");
+		System.out.println("Java ’ya Hos Geldiniz");
+	}
+}
---
+++ Task-1-HosgeldinMesajlari/üçmesaj.java
@@ -0,0 +1,10 @@
+
+public class üçmesaj {
+	public static void main(String[] args) {
+		System.out.println("Java'ya Hoş geldiniz");
+		System.out.println("Bilgisayar Bilimi'ne Hoş Geldiniz");
+		System.out.println("Programlama Eğlencelidir");
+				
+		
+	}
+}
